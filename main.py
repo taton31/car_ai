fef6211a8c9f85e725e057f345d3852cb0b2ec39
taton31 <sima9777@yandex.ru>
2022-12-18 16:21:27 +0300

    phis_car

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import numpy as np
 MAX_SPEED = 12.0
 MAX_SPEED_ROT = 3.0
 
-ALPHA = 0.01
+ALPHA = 1
 # How fast we accelerate
 ACCELERATION_RATE_START = 0.4
 ACCELERATION_RATE_STOP = 0.4
@@ -22,6 +22,15 @@ FRICTION = 0.02
 FRICTION_ROT = 0.1
 
 
+ENGINEFORCE = 1500000
+BRAKINGFORCE = ENGINEFORCE * 5
+C_DRAG = 2
+C_RR = 30 * C_DRAG
+MASS = 900
+WHEEL_ROT_PER_SEC = 0.32
+WHEEL_ROT_MAX = 3
+
+FRICTION_MAX = 100000
 
 SCREEN_WIDTH = 1850
 SCREEN_HEIGHT = 1000
@@ -34,134 +43,98 @@ class Car (arcade.Sprite):
         self.center_y = y
         self.center_x = x
 
+        self.L = math.sqrt( self.width**2 + self.height**2)
+        self.wheel_rot = 0
+
         self.up_pressed = False
         self.down_pressed = False
         self.left_pressed = False
         self.right_pressed = False
 
+        self.F_a = np.array([0, 0]) 
+        
         self.F_traction = np.array([1, 0]) 
         self.F_drag = np.array([1, 0]) 
         self.F_rr = np.array([1, 0]) 
         self.F_long = np.array([1, 0]) 
         
         self.direct = np.array([1, 0]) 
-        self.acc_module = 0
-        self.acc_module_rot = 0
-
-        self.vel_vector = np.array([1, 0]) 
-        self.vel_module = 0
-        self.vel_module_rot = 0
+        self.acc = np.array([0., 0.]) 
+        self.vel = np.array([0., 0.]) 
+        self.pos = np.array([x, y]) 
+        self.F_res_a = np.array([0., 0.]) 
+        
 
 
     def set_accel(self):
         pass
 
-    def rotate_Vec(self, angle):
-        rotatedX = self.direct[0] * math.cos(math.pi * angle/180) - self.direct[1] * math.sin(math.pi * angle/180)
-        rotatedY = self.direct[0] * math.sin(math.pi * angle/180) + self.direct[1] * math.cos(math.pi * angle/180)
-        self.direct = np.array([rotatedX, rotatedY]) 
+    def rotate_Vec(self, vec, angle):
+        angle = math.pi * angle / 180
+        rotatedX = vec[0] * math.cos(angle) - vec[1] * math.sin(angle)
+        rotatedY = vec[0] * math.sin(angle) + vec[1] * math.cos(angle)
+        return np.array([rotatedX, rotatedY]) 
+
+    def ang_to_Vec(self, angle):
+        angle = math.pi * angle / 180
+        rotatedX = math.cos(angle)
+        rotatedY = math.sin(angle)
+        return np.array([rotatedX, rotatedY]) 
+
+    def rotate_90(self, vec):
+        vel_len = self.len_vec(vec)
+        rotatedX = vec[1] / vel_len
+        rotatedY = - vec[0] / vel_len
+        return np.array([rotatedX, rotatedY]) 
+
+    def len_vec(self, vec):
+        return math.sqrt(vec.dot(vec))
 
 
     def update(self, delta_time):        
 
-        # if self.velocity_module > FRICTION:
-        #     self.velocity_module -= FRICTION
-        # elif self.velocity_module < -FRICTION:
-        #     self.velocity_module += FRICTION
-        # else:
-        #     self.velocity_module = 0
-
-        # if self.up_pressed and not self.down_pressed:
-        #     self.velocity_module += ACCELERATION_RATE
-        # elif self.down_pressed and not self.up_pressed:
-        #     self.velocity_module += -ACCELERATION_RATE
-        # if self.left_pressed and not self.right_pressed:
-        #     self.velocity_direction += ACCELERATION_RATE_ROT
-        # elif self.right_pressed and not self.left_pressed:
-        #     self.velocity_direction += -ACCELERATION_RATE_ROT
-
-
-        # if self.velocity_direction > FRICTION_ROT:
-        #     self.velocity_direction -= FRICTION_ROT
-        # elif self.velocity_direction < -FRICTION_ROT:
-        #     self.velocity_direction += FRICTION_ROT
-        # else:
-        #     self.velocity_direction = 0
-
-        # # Apply acceleration based on the keys pressed
-        
-
-        # if self.velocity_module > MAX_SPEED:
-        #     self.velocity_module = MAX_SPEED
-        # elif self.velocity_module < -MAX_SPEED:
-        #     self.velocity_module = -MAX_SPEED
-        # if self.velocity_direction > MAX_SPEED_ROT:
-        #     self.velocity_direction = MAX_SPEED_ROT
-        # elif self.velocity_direction < -MAX_SPEED_ROT:
-        #     self.velocity_direction = -MAX_SPEED_ROT
-
-        # self.change_angle = self.velocity_direction
-        # self.angle += self.change_angle
-
-        # self.change_x = math.cos(math.pi * self.angle/180) * self.velocity_module
-        # self.change_y = math.sin(math.pi * self.angle/180) * self.velocity_module
-        
-        # self.center_x += self.change_x
-        # self.center_y += self.change_y
-
-
         if self.up_pressed and not self.down_pressed:
-            self.acc_module = ACCELERATION_RATE_START
+            self.F_traction = self.direct * ENGINEFORCE
         elif self.down_pressed and not self.up_pressed:
-            self.acc_module = -ACCELERATION_RATE_STOP
+            self.F_traction = - self.direct * BRAKINGFORCE
+        else:
+            self.F_traction = 0
 
         if self.left_pressed and not self.right_pressed:
-            self.vel_module_rot = ACCELERATION_RATE_ROT
+            self.wheel_rot += WHEEL_ROT_PER_SEC
+            if WHEEL_ROT_MAX < self.wheel_rot: self.wheel_rot = WHEEL_ROT_MAX
         elif self.right_pressed and not self.left_pressed:
-            self.vel_module_rot = -ACCELERATION_RATE_ROT
-
-
-        self.rotate_Vec(self.vel_module_rot * delta_time)
-
-
-
-        if self.velocity_direction > FRICTION_ROT:
-            self.velocity_direction -= FRICTION_ROT
-        elif self.velocity_direction < -FRICTION_ROT:
-            self.velocity_direction += FRICTION_ROT
+            self.wheel_rot -= WHEEL_ROT_PER_SEC
+            if -WHEEL_ROT_MAX > self.wheel_rot: self.wheel_rot = -WHEEL_ROT_MAX
         else:
-            self.velocity_direction = 0
+            if math.fabs(self.wheel_rot) < WHEEL_ROT_PER_SEC:
+                self.wheel_rot = 0
+                pass
+            else:
+                self.wheel_rot += math.copysign(WHEEL_ROT_PER_SEC, -self.wheel_rot)
 
-            
-        if self.velocity_module > FRICTION:
-            self.velocity_module -= FRICTION
-        elif self.velocity_module < -FRICTION:
-            self.velocity_module += FRICTION
-        else:
-            self.velocity_module = 0
+        if self.wheel_rot != 0 and self.vel.dot(self.vel) != 0:
+            self.R = self.L / math.sin(self.wheel_rot) 
+            self.angle += self.wheel_rot
+            self.direct = self.ang_to_Vec(self.angle)
+        else: 
+            self.R = math.inf
+            self.F_a = np.array([0., 0.]) 
+            self.F_res_a = np.array([0., 0.]) 
 
-
-        # Apply acceleration based on the keys pressed
         
 
-        if self.velocity_module > MAX_SPEED:
-            self.velocity_module = MAX_SPEED
-        elif self.velocity_module < -MAX_SPEED:
-            self.velocity_module = -MAX_SPEED
-        if self.velocity_direction > MAX_SPEED_ROT:
-            self.velocity_direction = MAX_SPEED_ROT
-        elif self.velocity_direction < -MAX_SPEED_ROT:
-            self.velocity_direction = -MAX_SPEED_ROT
+        self.F_drag = - C_DRAG * self.len_vec(self.vel) * self.vel
+        self.F_rr = - C_RR * self.vel
 
-        self.change_angle = self.velocity_direction
-        self.angle += self.change_angle
+        self.F_long = self.F_traction + self.F_drag + self.F_rr + self.F_a + self.F_res_a
 
-        self.change_x = math.cos(math.pi * self.angle/180) * self.velocity_module
-        self.change_y = math.sin(math.pi * self.angle/180) * self.velocity_module
-        
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-        
+        self.acc = self.F_long / MASS
+        self.vel += self.acc * delta_time
+        self.pos += self.vel * delta_time
+        self.center_x = self.pos[0]
+        self.center_y = self.pos[1]
+         
 
 
         
@@ -188,10 +161,10 @@ class Welcome(arcade.Window):
         self.clear()
 
         self.Car.draw()
-        arcade.draw_text(f"vel_der: {self.Car.velocity_direction}", 10, 50, arcade.color.BLACK)
-        arcade.draw_text(f"vel_mod: {self.Car.velocity_module}", 10, 70, arcade.color.BLACK)
-        arcade.draw_text(f"ang: {self.Car.angle}", 10, 90, arcade.color.BLACK)
-        arcade.draw_text(f"FR: {FRICTION_ROT}", 10, 110, arcade.color.BLACK)
+        arcade.draw_text(f"FA: {self.Car.len_vec(self.Car.F_a)}", 10, 50, arcade.color.BLACK)
+        arcade.draw_text(f"FRESA: {self.Car.len_vec(self.Car.F_res_a)}", 10, 70, arcade.color.BLACK)
+        arcade.draw_text(f"FRICTION_MAX: {C_DRAG}", 10, 90, arcade.color.BLACK)
+        # arcade.draw_text(f"FR: {FRICTION_ROT}", 10, 110, arcade.color.BLACK)
         
 
     def on_update(self, delta_time: float = 1 / 60):
@@ -199,16 +172,16 @@ class Welcome(arcade.Window):
 
     def on_key_press(self, symbol, modifiers):
 
-        global FRICTION_ROT
+        global C_DRAG
         global ALPHA
 
         if symbol == arcade.key.O:
             # global FRICTION_ROT
-            FRICTION_ROT += ALPHA
+            C_DRAG += ALPHA
 
         if symbol == arcade.key.P:
             # global FRICTION_ROT
-            FRICTION_ROT -= ALPHA
+            C_DRAG -= ALPHA
 
         if symbol == arcade.key.UP:
             self.Car.up_pressed = True
